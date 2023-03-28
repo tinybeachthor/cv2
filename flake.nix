1cec4bade5bd7a5bed5cc633639e658bbc9398b8
Martin Toman <toman.martin@live.com>
2023-03-28 16:20:36 -0700

    Nix flake update

--- flake.nix
+++ flake.nix
@@ -1,18 +1,25 @@
 {
   inputs = {
-    flake-utils.url = github:numtide/flake-utils/master;
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
-    tinybeachthor = {
-      url = github:tinybeachthor/nur-packages/master;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = github:NixOS/nixpkgs;
   };
-  outputs = { self, flake-utils, nixpkgs, tinybeachthor }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs {
-          inherit system;
-          overlays = [ tinybeachthor.overlay ];
-        });
-      in { devShell = import ./shell.nix { inherit pkgs; }; });
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = (import nixpkgs {
+        inherit system;
+        overlays = [ ];
+      });
+    in {
+      devShell = with pkgs; mkShell {
+        buildInputs = [
+          git
+          gnumake
+
+          nodejs
+          yarn
+
+          rclone
+        ];
+      };
+    });
 }
